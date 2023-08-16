0d4372ea0e948b4977a129d30b66ea9b39dbde39
Katharine Zarbock <kjzarbock@gmail.com>
2023-08-15 21:36:24 -0500

    8/16 1328, added nav for games and events

---
+++ src/components/events/EventList.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import { getEvents } from "../../managers/EventManager.js";
+
+export const EventList = (props) => {
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        getEvents().then(data => setEvents(data));
+    }, []);
+
+    return (
+        <article className="events">
+            {
+                events.map(event => {
+                    return <section key={`event--${event.id}`} className="event">
+                        <div className="event__title">{event.title}</div>
+                        <div className="event__organizer">Organized by: {event.organizer.username}</div>
+                        <div className="event__game">Game: {event.game.title}</div>
+                        <div className="event__dateTime">Date & Time: {event.date_time}</div>
+                        <div className="event__location">Location: {event.location}</div>
+                    </section>
+                })
+            }
+        </article>
+    );
+};
